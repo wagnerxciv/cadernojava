c4d541b36208ee6b7cb552dc033931d482a0c75d
wagnerxciv <125404234+wagnerxciv@users.noreply.github.com>
2023-05-26 17:23:22 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("ola mundo");
+    int myNum = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = false;
+    String myText = "Muy Gay";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+
+    // parte dois do código
+
+    System.out.println(myText + myBool);
+
+    // codigo de transmissão
+
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; 
+    System.out.println(myDouble);  
+    System.out.println(myInt); 
+  }
+
+
+}
---
+++ main.kt
@@ -0,0 +1,21 @@
+fun main() {
+  println("oLA mUNDO!");
+  var nome: String = "Gaybriel"
+  val aniversario = 1975
+
+  println(nome)          // Print the value   of name
+  println(aniversario)     // Print the       value   of birthyear
+
+  val myNum: Int = 5                
+  val myDoubleNum: Double = 5.99   
+  val myLetter: Char = 'D'        
+  val myBoolean: Boolean = true    
+  val myText: String = "Hello"     
+
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
