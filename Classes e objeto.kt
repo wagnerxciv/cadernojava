49fc9529371048549aaa6a741cc53357a25a98c4
wagnerxciv <125404234+wagnerxciv@users.noreply.github.com>
2023-05-31 16:22:03 -0300

    Add files via upload

---
+++ Classes e objeto.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+ class Carro{
+   var marca = ""
+   var modelo =""
+   var ano = 2002
+}
+
+ val Celta = Carro()
+
+
+ Celta.marca ="Chevrolet"
+ Celta.modelo = "Celta"
+ Celta.ano = 2002
+
+  println( Celta.marca)
+  println( Celta.modelo)
+  println( Celta.ano)
+  }
